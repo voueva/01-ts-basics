d506f9c1076ea26c7a4c85143af4947d858389f9
voueva <voueva.e.v@gmail.com>
2025-06-19 13:17:42 +0300

    Task 3 - Java Script

---
+++ src/task-3.ts
@@ -0,0 +1,12 @@
+const usernames = ["alice", "bob", "charlie"];
+
+const ratings = [4.5, 3.8, 5];
+
+const products = [
+  { id: 1, title: "Phone" },
+  { id: 2, title: "Laptop" },
+];
+
+console.log(`Usernames: ${JSON.stringify(usernames)}`);
+console.log(`Ratings: ${JSON.stringify(ratings)}`);
+console.log(`Products: ${JSON.stringify(products)}`);
