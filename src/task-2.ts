02819b4802ef23a5fb6e9b64d73eea592fc03e49
voueva <voueva.e.v@gmail.com>
2025-06-19 13:14:03 +0300

    Task 2 - Java Script

--- src/task-2.ts
+++ src/task-2.ts
@@ -1,14 +1,8 @@
-interface Product {
-    readonly id: number;
-    title: string;
-    description?: string;
-}
-
-const product: Product = {
+const product = {
     id: 1,
     title: "Tablet",
     description: "Compact and fast",
-};
-
-console.log(`Product: ${JSON.stringify(product)}`);
+  };
+  
+  console.log(`Product: ${JSON.stringify(product)}`);
   
