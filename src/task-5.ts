3cee310ac40873d14a9f45a61eb00218024559d8
voueva <voueva.e.v@gmail.com>
2025-06-19 15:05:07 +0300

    Task 5 - Java Script

---
+++ src/task-5.ts
@@ -0,0 +1,12 @@
+function logStatus(status) {
+    if (status === "loading") {
+      console.log("Loading...");
+    } else if (status === "success") {
+      console.log("Success!");
+    } else if (status === "error") {
+      console.log("Something went wrong");
+    }
+  }
+  
+  logStatus("loading");
+  
