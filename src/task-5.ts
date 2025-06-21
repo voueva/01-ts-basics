212f830e60794827fb3cc1e15970caed140f23fc
voueva <voueva.e.v@gmail.com>
2025-06-21 16:20:59 +0300

    Task 5 - Type Script

--- src/task-5.ts
+++ src/task-5.ts
@@ -1,4 +1,4 @@
-function logStatus(status) {
+function logStatus(status: "loading" | "success" | "error"): void {
     if (status === "loading") {
       console.log("Loading...");
     } else if (status === "success") {
@@ -8,5 +8,7 @@ function logStatus(status) {
     }
   }
   
-  logStatus("loading");
+logStatus("loading");
+logStatus("success");
+logStatus("error");
   
