f6ddfcaec5834de46982e2b80111539f32868384
voueva <voueva.e.v@gmail.com>
2025-06-23 13:48:45 +0300

    Task 8 - Type Script

--- src/task-8.ts
+++ src/task-8.ts
@@ -1,7 +1,13 @@
 import axios from "axios";
 
-async function fetchPosts() {
-  const response = await axios.get(
+interface Post {
+    id: number;
+    title: string;
+    body: string;
+}
+
+async function fetchPosts(): Promise<Post[]> {
+  const response = await axios.get<Post[]>(
     'https://jsonplaceholder.typicode.com/posts'
   );
   return response.data;
